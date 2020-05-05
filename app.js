95e9e328643c1b4471cfe8ea8f85c8149741543b
technicalclub-rcoem <technicalclub-rcoem>
2020-05-05 12:09:31 +0530

    app.js : mouse event added

---
+++ app.js
@@ -0,0 +1,12 @@
+Stage(function(stage){
+
+	stage.viewbox(50,50).pin('handle',-0.5);
+
+	stage.on('click',function(){ 		// Mouse-Click : stage
+		console.log("MouseClicked"); 
+	});
+
+	ddsjkjrf
+
+});
+
